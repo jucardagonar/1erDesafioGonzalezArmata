d8888e05d4dcb33fc7f766556612c94fa1c26f45
Juan Carlos González Armata <jucardagonar@gmail.com>
2023-06-15 21:23:42 -0300

    cambio de title

--- myecommerce/App.js
+++ myecommerce/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HOLA CODER</Text>
+      <Text>HOLA CODER/Text>
       <StatusBar style="auto" />
     </View>
   );
